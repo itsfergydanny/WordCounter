624a5f691310fb6c73032d0474db72206f70def6
tyldanny <99778985+tyldanny@users.noreply.github.com>
2022-05-25 22:58:37 -0400

    upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# WordCounter
+
+<br>
+
+Requirements: 
+* Java 17
+* Maven
+
+<br>
+
+Hi, here is my solution for the assessment. You can compile the project using Maven by running the following while in the project root folder:
+```shell
+mvn package
+```
+
+<br>
+
+
+Once compiled, you have two options when it comes to running the jar. One is that you provide the file path as an argument, for example:
+```shell
+java -jar ./target/WordCounter-1.0-SNAPSHOT-shaded.jar test.txt
+```
+
+<br>
+
+The second option is by simply running the jar and then typing the file path and hitting enter:
+```shell
+java -jar ./target/WordCounter-1.0-SNAPSHOT-shaded.jar
+```
+
---
+++ src/main/java/com/tyldanny/wordcounter/Main.java
@@ -0,0 +1,92 @@
+package com.tyldanny.wordcounter;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String path = args[0];
+            File file = validateFile(path);
+            if (file == null) {
+                return;
+            }
+            readFile(file);
+            return;
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter the file path of the file you want scanned:");
+
+        try {
+            String input = reader.readLine();
+            if (input == null) {
+                System.out.println("Unable to read file. Make sure you are pointing to a valid file.");
+                return;
+            }
+            String path = input.split("\\s+")[0];
+            File file = validateFile(path);
+            if (file == null) {
+                return;
+            }
+            readFile(file);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static File validateFile(String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            System.out.println("No file found at \"" + path + "\"");
+            return null;
+        }
+        if (!file.isFile()) {
+            System.out.println("The path mentioned points to a directory. It must point to a file!");
+            return null;
+        }
+        if (!file.canRead()) {
+            System.out.println("Unable to read file at \"" + path + "\"");
+            return null;
+        }
+        return file;
+    }
+
+    private static void readFile(File file) {
+        Map<String, Integer> map = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] words = line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+                for (String word : words) {
+                    map.put(word, map.getOrDefault(word, 0) + 1);
+                }
+                line = br.readLine();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        printResult(getMapOrderedByValue(map));
+    }
+
+    private static void printResult(Map<String, Integer> result) {
+        StringBuilder str = new StringBuilder();
+        for (String key : result.keySet()) {
+            str.append(key).append(": ").append(result.get(key)).append("\n");
+        }
+        System.out.println(str);
+    }
+
+    private static Map<String, Integer> getMapOrderedByValue(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+
+        Map<String, Integer> sorted = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sorted.put(entry.getKey(), entry.getValue());
+        }
+        return sorted;
+    }
+}
